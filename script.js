1ab331ca46e0efdeef414043b57f0d71b5addc03
Istefane Guilherme do Nascimento <art0066383@aluno.firjansenaisesi.com.br>
2026-03-02 16:31:39 -0300

    POKEAPI

---
+++ script.js
@@ -0,0 +1,31 @@
+async function buscarPokemon() {
+    const valor = document.getElementById("pokemonInput").value.toLowerCase();
+
+    if (!valor) {
+        alert("Digite o nome ou número do Pokémon!");
+        return;
+    }
+
+    try {
+        const resposta = await fetch(`https://pokeapi.co/api/v2/pokemon/${valor}`);
+        const dados = await resposta.json();
+
+        // Nome + número
+        document.getElementById("pokemonInfo").innerText =
+            `${dados.name} #${dados.id}`;
+
+        // GIF animado (generation V)
+        document.getElementById("pokemonImg").src =
+            dados.sprites.versions["generation-v"]["black-white"].animated.front_default;
+
+        // Ataques
+        document.getElementById("ataque1").innerText =
+            `ATAQUE 1: ${dados.abilities[0].ability.name.toUpperCase()}`;
+
+        document.getElementById("ataque2").innerText =
+            `ATAQUE 2: ${dados.abilities[1]?.ability.name.toUpperCase() || "N/A"}`;
+
+    } catch (erro) {
+        alert("Pokémon não encontrado!");
+    }
+}
